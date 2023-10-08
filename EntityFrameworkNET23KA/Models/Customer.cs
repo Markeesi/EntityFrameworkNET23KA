989589fdeba9b22d7e1aa0825be7b3a3a03700ec
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-10-08 17:12:56 +0300

    Connection established

---
+++ EntityFrameworkNET23KA/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkNET23KA.Models;
+
+public partial class Customer
+{
+    public int CustomerId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Location { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+}
---
+++ EntityFrameworkNET23KA/Models/MasterContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkNET23KA.Models;
+
+public partial class MasterContext : DbContext
+{
+    public MasterContext()
+    {
+    }
+
+    public MasterContext(DbContextOptions<MasterContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost,57000;Database=master;User Id=sa;Password=Str#ng_Passw#rd;Trusted_Connection=False;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PK__Customer__A4AE64D82090B1B4");
+
+            entity.Property(e => e.CustomerId).ValueGeneratedNever();
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.Location).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
