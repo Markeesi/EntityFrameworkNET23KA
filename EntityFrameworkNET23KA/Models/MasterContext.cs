7ca311965287cd30bbe9e02e4dda6599d5da3fc5
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-10-08 19:07:12 +0300

    DB Connection

--- EntityFrameworkNET23KA/Models/MasterContext.cs
+++ EntityFrameworkNET23KA/Models/MasterContext.cs
@@ -1,40 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
-namespace EntityFrameworkNET23KA.Models;
-
-public partial class MasterContext : DbContext
+namespace EntityFrameworkNET23KA.Models
 {
-    public MasterContext()
+    public partial class MasterContext : DbContext
     {
-    }
-
-    public MasterContext(DbContextOptions<MasterContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<Customer> Customers { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,57000;Database=master;User Id=sa;Password=Str#ng_Passw#rd;Trusted_Connection=False;TrustServerCertificate=True");
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Customer>(entity =>
+        public MasterContext(DbContextOptions<MasterContext> options)
+            : base(options)
         {
-            entity.HasKey(e => e.CustomerId).HasName("PK__Customer__A4AE64D82090B1B4");
+        }
 
-            entity.Property(e => e.CustomerId).ValueGeneratedNever();
-            entity.Property(e => e.Email).HasMaxLength(50);
-            entity.Property(e => e.Location).HasMaxLength(50);
-            entity.Property(e => e.Name).HasMaxLength(50);
-        });
+        public MasterContext() { }
 
-        OnModelCreatingPartial(modelBuilder);
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile(@"C:\\Users\\Markk\\source\\repos\\EntityFrameworkNET23KA\\EntityFrameworkNET23KA\\appsettings.json")
+                    .Build();
+
+                string connectionString = configuration.GetConnectionString("MyConnection");
+
+                if (string.IsNullOrWhiteSpace(connectionString))
+                {
+                    Console.WriteLine("Connection string not found in appsettings.json.");
+                    return;
+                }
+
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(e => e.CustomerId).HasName("PK__Customer__A4AE64D82090B1B4");
+
+                entity.Property(e => e.CustomerId).ValueGeneratedNever();
+                entity.Property(e => e.Email).HasMaxLength(50);
+                entity.Property(e => e.Location).HasMaxLength(50);
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+        }
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
--- EntityFrameworkNET23KA/Program.cs
+++ EntityFrameworkNET23KA/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using EntityFrameworkNET23KA.Models;
+
+using var db = new MasterContext();
+
+var customer = db.Customers
+    .OrderBy(b => b.CustomerId)
+    .First();
+
+Console.WriteLine("Customer Details: " + customer.Name);
+
+
+var customers = db.Customers.ToList();
+
+foreach (var customerItem in customers)
+{
+    Console.WriteLine("Customer Details: " + customerItem.Name);
+}
